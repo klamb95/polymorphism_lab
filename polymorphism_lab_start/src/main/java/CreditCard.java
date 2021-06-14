d8b6152322c43a1390c7ed00be6fd218cb29ef45
klamb95 <klamb1995@gmail.com>
2021-06-14 14:06:04 +0100

    first commit

---
+++ polymorphism_lab_start/src/main/java/CreditCard.java
@@ -0,0 +1,8 @@
+public class CreditCard extends PaymentCard{
+
+
+    public CreditCard(String cardNumber, String expiryDate, int securityNumber) {
+        super(cardNumber, expiryDate, securityNumber);
+    }
+
+}
